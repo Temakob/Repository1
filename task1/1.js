bc22dd3dc9557244c29ce0f69ff6264c8560b617
Temakob <87011315+Temakob@users.noreply.github.com>
2021-08-25 15:38:47 +0300

    Add files via upload

--- task1/1.js
+++ task1/1.js
@@ -2,15 +2,22 @@
 	"use strict";
 
 	function CalculateString(inputStr) {
-		this._str = inputStr;
-		alert("Введена строка: " + this._str);
+		this._str = inputStr; 
+        //Технически также возможно получить доступ к объекту без ключевого слова this, ссылаясь на него через внешнюю переменную
+        //(в которой хранится ссылка на этот объект): …Но такой код будет ненадёжным.
+		console.log("Введена строка: " + this._str);
 	}
 	CalculateString.prototype.calc = function() {
-		let result = 0,
-			matchArr = [],
-			searchPattern = /\-?\d+(\.\d+)?|[\+,\-,\*,\/,\=]{1}/ig;
+        // Свойство F.prototype (не путать с [[Prototype]]) устанавливает[[Prototype]] для новых объектов при вызове new F().
+        // Добавляем свойство calc и присваиваем функцию
+		let result = 0, //почему ноль?
+			matchArr = [], 
+			searchPattern = /\-?\d+(\.\d+)?|[\+,\-,\*,\/,\=]{1}/ig; // символьные классы
+			// В следующем примере метод match() используется для поиска символов, за которыми следуют 1 или более цифр, за которыми следуют 0 или более раз группы из точки и цифры. 
+			// Регулярное выражение включает флаг i и g, так что регистр букв игнорируется и возвращаются все совпадения.
+			// почему "?"?
 
-		matchArr = this._str.match(searchPattern);
+		matchArr = this._str.match(searchPattern); //зачем нижнее подчеркивание?
 
 		if(matchArr[0]*1+"" !== "NaN") {
 			result += matchArr[0]*1;
@@ -18,12 +25,16 @@
 
 		for(let i = 0; i < matchArr.length; i++) {
 			switch(matchArr[i]) {
+				//Конструкция switch заменяет собой сразу несколько if. Она представляет собой более наглядный способ сравнить выражение сразу с несколькими вариантами.
 				case "+": result += matchArr[i+1] * 1; break;
+                //let n = 2;
+                //    n += 5; теперь n = 7 (работает как n = n + 5)
+                //    n *= 2; теперь n = 14 (работает как n = n * 2)
 				case "-": result -= matchArr[i+1] * 1; break;
 				case "*": result *= matchArr[i+1] * 1; break;
 				case "/": result /= matchArr[i+1] * 1; break;
 				case "=": break;
-				default: continue; break;
+				default: continue; break; //почему и continue и break?
 			}
 		}
 
@@ -31,13 +42,13 @@
 	}
 
 	let test = new CalculateString("3.5 землекопа +4 поросенка *10 рублей - 5.5 $ /5 человек =");
-	alert(test.calc().toFixed(2));
+	console.log(test.calc().toFixed(2));
 
 	let test2 = new CalculateString("7+7*2=ёжик");
-	alert(test2.calc().toFixed(2));
+	console.log(test2.calc().toFixed(2));
 
 	let test3 = new CalculateString("2 яблока + апельсин -2 жука + 4 куропатки = ");
-	alert(test3.calc().toFixed(2));
+	console.log(test3.calc().toFixed(2));
 
 })();
 
