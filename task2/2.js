74e6a7ab134cd4db18d5a2df8afee307202835b4
Temakob <87011315+Temakob@users.noreply.github.com>
2021-08-25 15:39:05 +0300

    Add files via upload

--- task2/2.js
+++ task2/2.js
@@ -5,23 +5,25 @@
 		if(typeof(inputStr) === "string") {
 			this._str = inputStr;
 		} else {
-			throw "Ошибка! Вводимые данные должны быть строкой.";
+			throw "Ошибка! Вводимые данные должны быть строкой."; //Оператор throw генерирует ошибку.
 		}
 	}
 	RemoverChars.prototype.removeChars = function() {
         let result = this._str,
-			matchArr = [],
-			matchPattern = /[^\s,\.,\,,\:,\;,\!,\?]+/gi,
+			matchArr = [], //почему такие скобки?
+			matchPattern = /[^\s,\.,\,,\:,\;,\!,\?]+/gi, //i - искать во всех регистрах, g - возвращать все совпадения
 			firstWord = "";
 
 		if(!/[^\s,\.,\,,\:,\;,\!,\?]+/i.test(result)) { return result; }
 		matchArr = this._str.match(matchPattern);
 
-		if(matchArr.length === 1) { return result; }
+		if(matchArr.length === 1) { return result; } //== сравнение на равенство, === сравнение на идентичность
 		firstWord = matchArr[0];
 
-		for(let j=0; j<firstWord.length; j++) {
-			let searchChar = new RegExp(firstWord[j], "i");
+		for(let j=0; j<firstWord.length; j++) { //j это переменная, а j++ это увеличение числа в переменной на 1 при каждой итерации цикла
+			let searchChar = new RegExp(firstWord[j], "i"); 
+			//Регулярные выражения – мощное средство поиска и замены в строке.
+			//В JavaScript регулярные выражения реализованы отдельным объектом RegExp и интегрированы в методы строк.
 			let count = 0;
 			for(let i=0; i<matchArr.length; i++) {
 				if(searchChar.test(matchArr[i])) {
